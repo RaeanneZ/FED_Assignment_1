08815e052ec2026548a0769a4cb1252ff9e9203b
RaeanneZ <raeannezou@gmail.com>
2023-11-29 10:02:03 +0800

    Fixed home page carousel

---
+++ testimonialCarousel.js
@@ -0,0 +1,19 @@
+let slides = document.querySelectorAll(".item");
+let dots = document.querySelectorAll("li");
+let index = 0;
+
+function next() {
+  slides[index].classList.remove("active");
+  dots[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+  dots[index].classList.add("active");
+}
+
+function prev() {
+  slides[index].classList.remove("active");
+  dots[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+  dots[index].classList.add("active");
+}
