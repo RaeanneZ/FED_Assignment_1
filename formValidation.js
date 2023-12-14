ee4cc0b8e7e2e6a843f95046f029c88ee0ca456f
RaeanneZ <raeannezou@gmail.com>
2023-12-14 20:52:00 +0800

    payment page redirects to successful notice page

--- formValidation.js
+++ formValidation.js
@@ -10,7 +10,9 @@ const expDate = document.getElementById("expDate");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  validateInput();
+  if (validateInput()) {
+    window.location.href = "successfulOrderPage.html";
+  }
 });
 
 // Ensure expiration date is not earlier than the next day
@@ -29,51 +31,63 @@ function validateInput() {
   const cardNoValue = cardNo.value.trim();
   const expDateValue = expDate.value; // YYYY-MM-DD
 
-  console.log(expDateValue);
+  let isValid = true;
 
   if (fNameValue === "") {
     setError(fname, "First Name is required");
+    isValid = false;
   } else {
     setSuccess(fname);
   }
 
   if (lNameValue === "") {
     setError(lname, "Last Name is required");
+    isValid = false;
   } else {
     setSuccess(lname);
   }
 
   if (emailValue === "") {
     setError(email, "Email is required");
+    isValid = false;
   } else if (!isValidEmail(emailValue)) {
     setError(email, "Please provide valid email address");
+    isValid = false;
   } else {
     setSuccess(email);
   }
 
   if (postalCodeValue === "") {
     setError(postalCode, "Postal Code is required");
+    isValid = false;
   } else if (isNaN(Number(postalCodeValue)) || postalCodeValue.length != 5) {
     setError(postalCode, "Please enter valid US Postal Code");
+    isValid = false;
   } else {
     setSuccess(postalCode);
   }
 
   if (phoneNoValue === "") {
     setError(phoneNo, "Phone number is required to verify order and delivery");
+    isValid = false;
   } else if (isNaN(Number(phoneNoValue))) {
     setError(phoneNo, "Please enter phone number");
+    isValid = false;
   } else {
     setSuccess(phoneNo);
   }
 
   if (cardNoValue === "") {
     setError(cardNo, "Card number");
+    isValid = false;
   } else if (isNaN(Number(cardNoValue)) || cardNoValue.length != 16) {
     setError(cardNo, "Invalid Card Number");
+    isValid = false;
   } else {
     setSuccess(cardNo);
   }
+
+  return isValid;
 }
 
 function isValidEmail(email) {
